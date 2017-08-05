1cce9359f4c48f89f732e66531627775d523c61f
NLOG <sorisem4106@naver.com>
2017-08-05 15:11:37 +0900

    손에 들고있는 아이템으로 전체 지급 가능

--- src/nlog/NLOGGiveAll/Main.php
+++ src/nlog/NLOGGiveAll/Main.php
@@ -4,7 +4,6 @@ namespace nlog\NLOGGiveAll;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\event\Listener;
-use pocketmine\utils\TextFormat;
 use pocketmine\command\CommandSender;
 use pocketmine\command\Command;
 use pocketmine\item\Item;
@@ -22,34 +21,37 @@ class Main extends PluginBase implements Listener{
  	 	
  	 	if ($cmd->getName() === "giveall") {}
  	 		if(!(count($args) === 1 or count($args) === 2)) {
- 	 			$sender->sendMessage("§o§b[ 알림 ] §7/giveall [아이템 코드] [아이템 수량]");
+ 	 			$sender->sendMessage("§o§b[ 알림 ] §7/giveall <아이템 코드> [아이템 수량]");
  	 			return;
  	 		}
 			if (!(isset ($args[1]))) {
 				$args[1] = 1;
 			}
- 	 		$item = Item::fromString($args[0]);
- 	 		$id = $item->getId();
- 	 		$name = $item->getName();
- 	 		$online = $this->getServer()->getOnlinePlayers();
 			
- 	 		if ($args[0] <= 0 or $args[0] >= 32766) {
- 	 			$sender->sendMessage("§o§b[ 알림 ] §7존재하지 않는 아이템입니다.");
+			if ($args[0] === "hand") {
+				$item = $this->getServer()->getPlayerExact($sender->getName())->getInventory()->getItemInHand();
+			}else{
+ 	 			$item = Item::fromString($args[0]);
+			}
+			
+			$item->setCount((int)$args[1]);
+			
+			
+			if ($item->getId() === 0) {
+				$sender->sendMessage("§o§b[ 알림 ] §7알 수 없는 아이템입니다.");
 				return;
- 	 		}
+			}
+ 	 		
 			if ($args[1] <= 0 or $args[1] > 64) {
  	 			$sender->sendMessage("§o§b[ 알림 ] §7개수는 1개 이상 64개 이하로 입력해주세요.");
 				return;
 			}
-			if (count($online) === 0) {
- 	 			$sender->sendMessage("§o§b[ 알림 ] §7서버에 아무도 없습니다.");
-				return;
-			else{
- 	 			foreach ($online as $player) {
- 	 				$player->getInventory()->addItem(new Item($id,$item->getDamage(),$args[1]));
- 	 			}
- 	 			$this->getServer()->broadcastMessage("§o§b[ 알림 ] §7모든 플레이어에게 " . $name . " 을(를) " . $args[1] . " 만큼 지급하였습니다.");
+			
+ 	 		foreach ($this->getServer()->getOnlinePlayers() as $player) {
+ 	 			$player->getInventory()->addItem($item);
  	 		}
+ 	 		
+ 	 		$this->getServer()->broadcastMessage("§o§b[ 알림 ] §7모든 플레이어에게 " . $item->getName() . " 을(를) " . $args[1] . " 만큼 지급하였습니다.");
  	 	}
  	 }
 ?>
